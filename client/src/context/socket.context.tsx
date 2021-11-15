f1ca3c6662813b5b07313c536b585837f705a1fa
Jakub Vala <jamm3e3333@gmail.com>
2021-11-15 11:58:13 +0100

    Create provider

---
+++ client/src/context/socket.context.tsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from 'react';
+import io from 'socket.io-client';
+import { SOCKET_URL } from '../config/default';
+
+const SocketContext = createContext({});
+
+ function SocketsProvider(props: any) {
+    return (
+        <SocketContext.Provider
+            value={{}}
+        >
+            {...props} 
+        </SocketContext.Provider>
+    )
+ }
+
+export const useSockets =  () => useContext(SocketContext);
+
+ export default SocketsProvider; 
--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,7 +1,16 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import logger from './utils/logger';
+
+const EVENTS = {
+    CLIENT: {
+        CONNECT: 'connection',
+    }
+}
 
 const socket = ({io}: {io: Server}) => {
-          
+    io.on(EVENTS.CLIENT.CONNECT, (socket: Socket) => {
+        logger.info(`Connection: ${socket}`);
+    });
 }
 
 export default  socket;
